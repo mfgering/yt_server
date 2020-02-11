efa140d7d335c12c518684cd320c9de1de85b35d
Mike Gering <mike@mfg-consulting-llc.com>
2020-02-11 16:12:45 -0500

    local_settings

--- config.py
+++ config.py
@@ -12,8 +12,10 @@ import local_settings
 class Config(object):
 	SECRET_KEY = os.environ.get("SECRET_KEY") or "sske89822Jl..234BBB--=SSS298234"
 	MAX_CONCURRENT_DL = 3
-	FFMPEG_LOCATION = local_settings.FFMPEG_LOCATION
-	TEMPLATES_AUTO_RELOAD = local_settings.TEMPLATES_AUTO_RELOAD
-	DEFAULT_DOWNLOAD_DIR = local_settings.DEFAULT_DOWNLOAD_DIR
-	DEFAULT_DOWNLOAD_NAME_PATTERN = local_settings.DEFAULT_DOWNLOAD_NAME_PATTERN
+	TEMPLATES_AUTO_RELOAD = False
+	DEFAULT_DOWNLOAD_NAME_PATTERN = '%(title)s.%(ext)s'
+	FFMPEG_LOCATION = None
 
+_local_members = [attr for attr in dir(local_settings) if not callable(getattr(local_settings, attr)) and not attr.startswith("__")]
+for local_member in _local_members:
+	setattr(Config, local_member, getattr(local_settings, local_member))
