fe980110f4bc3367eca834a8c75d5ccd20946d84
Mike Gering <mike@mfg-consulting-llc.com>
2020-02-13 17:12:28 -0500

    dir validation

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
+from pathlib import Path
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, URL
+from wtforms.validators import DataRequired, URL, ValidationError
 
 class LoginForm(FlaskForm):
 	username = StringField('Username', validators=[DataRequired()])
@@ -13,3 +14,16 @@ class DownloadForm(FlaskForm):
 	dl_dir = StringField('Download directory', validators=[DataRequired(message='A directory name is needed')])
 	dl_patt = StringField('Download pattern', validators=[DataRequired(message='A pattern is needed')])
 	submit = SubmitField('Submit')
+
+	@staticmethod
+	def validate_dl_dir(form, field):
+		path1 = Path(field.data)
+		if path1.is_file():
+			raise ValidationError("Should be a directory, not a file.")
+		if path1.is_dir():
+			#TODO: Can write file in the directory?
+			return
+		try:
+			path1.mkdir(parents=True, exist_ok=False)
+		except Exception as exc:
+			raise ValidationError("Could not create the directory: "+str(exc))
