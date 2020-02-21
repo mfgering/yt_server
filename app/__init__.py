8b0b3335feb5147120e6daae15b3b96ef62a553f
Mike Gering <mike@mfg-consulting-llc.com>
2020-02-21 14:28:29 -0500

    uptime

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from config import Config
+import datetime
 
 app = Flask(__name__)
 app.config.from_object(Config)
+app.start_time = datetime.datetime.utcnow()
 
 from app import routes
--- run.py
+++ run.py
@@ -1,5 +1,4 @@
 from app import app
-import datetime
-app.start_time = datetime.datetime.utcnow()
+
 if __name__ == "__main__":
     app.run()
